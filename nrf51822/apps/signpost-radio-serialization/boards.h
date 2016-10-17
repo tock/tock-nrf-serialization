7d161029362b26b3951a1f55ffbaf2f31ec2592b
Josh Adkins <adkinsjd@umich.edu>
2016-10-16 17:47:19 -0700

    added the signpost serialization app

---
+++ nrf51822/apps/signpost-radio-serialization/boards.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define LEDS_NUMBER 1
+
+#define SER_CON_RX_PIN              11   // UART RX pin number.
+#define SER_CON_TX_PIN              12   // UART TX pin number.
+#define SER_CON_CTS_PIN             9 // UART Clear To Send pin number.
+#define SER_CON_RTS_PIN             10   // UART Request To Send pin number.
+
+#define LED_0          18
